d50c181592c5d1cef6d9e2d3168253e37ab0b8d4
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2023-06-01 20:12:18 -0300

    Inclusão de estrutura do projeto

---
+++ 4 - Projeto/MiniWineBar/src/main/java/br/com/wineSquad/wineBar/domain/Dados/DadosCriar.java
@@ -0,0 +1,5 @@
+package main.java.br.com.wineSquad.wineBar.domain.Dados;
+
+public class DadosCriar {
+
+}
---
+++ 4 - Projeto/MiniWineBar/src/main/java/br/com/wineSquad/wineBar/domain/MiniWineBarApp.java
@@ -0,0 +1,7 @@
+package main.java.br.com.wineSquad.wineBar.domain;
+
+public class MiniWineBarApp {
+    public static void main(String[] args) {
+        
+    }
+}
